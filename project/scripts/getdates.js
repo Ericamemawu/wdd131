39223cf4b19e31bd1fa9be13dd1531d96cc64db7
amemawueric <amemawueric199999@gmail.com>
2026-01-14 03:29:39 +0000

    update

--- project/scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-// GET THE CURRENT YEAR
-const currentYear = new Date().getFullYear();
-
-// POPULATE HTMLS ELEMENTS WITH CURRENT YEAR AND LAST MODIFIED DATE
-document.getElementById("currentyear").textContent = currentYear;
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
